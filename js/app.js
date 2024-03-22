efd839f3f99b2e0c1715305afc907513b2041666
Mandeep-Dhillon <Mandeep.Dhillon@triosstudent.com>
2024-03-22 18:59:18 -0400

    added the js file

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+// Select the body element
+const body = document.querySelector('body');
+
+// Apply CSS reset styles to the body element
+body.style.boxSizing = 'border-box';
+body.style.margin = '0';
+body.style.padding = '0';
+body.style.border = '0';
